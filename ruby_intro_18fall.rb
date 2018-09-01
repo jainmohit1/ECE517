9e8f38ec5a1a207e03f925d0018f011b9c4d1e22
mjain6 <mjain6@ncsu.edu>
2018-09-01 17:22:33 -0400

    Initial Template

---
+++ ruby_intro_18fall.rb
@@ -0,0 +1,33 @@
+# Part 1
+
+def unique_array(a)
+  # ADD YOUR CODE HERE
+end
+
+def two_sum?(a,n)
+  # ADD YOUR CODE HERE
+end
+
+def group_anagrams(a)
+  # ADD YOUR CODE HERE
+end
+
+# Part 2
+
+def palindrome?(s)
+  # ADD YOUR CODE HERE
+end
+
+def remove_and_append_vowels(s)
+  # ADD YOUR CODE HERE
+end
+
+def highest_frequency_word(s)
+  # ADD YOUR CODE HERE
+end
+
+# Part 3
+
+class Book
+  # ADD YOUR CODE HERE
+end
