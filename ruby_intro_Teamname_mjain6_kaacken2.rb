193c3bea37ec2c1a86ff134f787110a236b52c4b
mjain6 <mjain6@ncsu.edu>
2018-09-01 22:12:13 -0400

    Question 2
    For two_sum function, the proposed solution works but I am still looking for optimal solution.

--- ruby_intro_Teamname_mjain6_kaacken2.rb
+++ ruby_intro_Teamname_mjain6_kaacken2.rb
@@ -2,17 +2,27 @@
 
 def unique_array(a)
   # ADD YOUR CODE HERE
+  # Using uniq! so that the original array itself is changed and will return only unique elements
+  a.uniq!
 end
 
-def two_sum?(a,n)
+def two_sum?(a, n)
   # ADD YOUR CODE HERE
+  a.uniq.each  do |element|
+    a.uniq.each do |y|
+      if (y!=element)
+        return true if n==y+element
+      end
+    end
+  end
+  false
 end
 
 def group_anagrams(a)
   # ADD YOUR CODE HERE
 end
 
-# Part 2
+# Part 2 
 
 def palindrome?(s)
   # ADD YOUR CODE HERE
