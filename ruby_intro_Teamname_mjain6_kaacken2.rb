fccff1aa4956ebdd74d7da381eece7e139db5442
Kevin Acken <kaacken2@ncsu.edu>
2018-09-04 21:55:46 -0400

    add question 3 solution and alternate solution to question 2

--- ruby_intro_Teamname_mjain6_kaacken2.rb
+++ ruby_intro_Teamname_mjain6_kaacken2.rb
@@ -1,13 +1,15 @@
 # Part 1
 
 def unique_array(a)
-  # ADD YOUR CODE HERE
   # Using uniq! so that the original array itself is changed and will return only unique elements
   a.uniq!
 end
 
 def two_sum?(a, n)
-  # ADD YOUR CODE HERE
+  # Alternate solution
+  #
+  # return a.combination(2).to_a.map! {|x,y| (x + y) == n}.count(true) > 0
+
   a.uniq.each  do |element|
     a.uniq.each do |y|
       if (y!=element)
@@ -19,7 +21,25 @@ def two_sum?(a, n)
 end
 
 def group_anagrams(a)
-  # ADD YOUR CODE HERE
+  groups_of_anagrams = Array.new
+
+  while not a.empty?
+    anagram_group = Array.new
+    word = a.delete_at(0)
+    anagram_group.insert(0, word)
+
+    a.each do |other_word|
+      condition = word.chars.permutation.to_a.count(other_word.chars);
+      if word.chars.permutation.to_a.count(other_word.chars) > 0 then
+        anagram_group.insert(-1, other_word)
+        a.delete(other_word)
+      end
+    end
+
+    groups_of_anagrams.insert(-1, anagram_group)
+  end
+
+  return groups_of_anagrams
 end
 
 # Part 2 
